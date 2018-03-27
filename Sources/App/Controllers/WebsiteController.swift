78d0c27d8d789878ee5dcac784c6f6fb4bc3e8c4
Mehmet Koca <mehmetkoca@outlook.com>
2018-03-27 22:32:34 +0300

    embeded templates

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -8,6 +8,8 @@ struct WebsiteController: RouteCollection {
         router.get(use: index)
         // song sayfası için kullanılacak route yapısı
         router.get("songs", Song.parameter, use: songHandler)
+        // user sayfası için kullanılacak route yapısı
+        router.get("users", User.parameter, use: userHandler)
     }
     
     // index page için kullanacağımız fonksiyon
@@ -16,7 +18,7 @@ struct WebsiteController: RouteCollection {
         return Song.query(on: req).all().flatMap(to: View.self) { songs in
             // Content'in title'ına atama yapıyoruz.
             // IndexContent yaratırken songs için kontrolü de yapıyoruz
-            let context = IndexContent(title: "Homepage", songs: songs.isEmpty ? nil : songs)
+            let context = IndexContext(title: "Homepage", songs: songs.isEmpty ? nil : songs)
             // 1. parametre: index.leaf'ı render et, 2. parametre: context'i kullan
             return try req.leaf().render("index", context)
         }
@@ -26,8 +28,18 @@ struct WebsiteController: RouteCollection {
     func songHandler(_ req: Request) throws -> Future<View> {
         return try req.parameter(Song.self).flatMap(to: View.self) { song in
             return try song.creator.get(on: req).flatMap(to: View.self) { creator in
-                let context = SongContent(title: song.title, song: song, creator: creator)
+                let context = SongContext(title: song.title, song: song, creator: creator)
                 return try req.leaf().render("song",context)
+            }
+        }
+    }
+    
+    // linklendirdiğimiz user sayfası için kullanılacak fonksiyon
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameter(User.self).flatMap(to: View.self) { user in
+            return try user.songs.query(on: req).all().flatMap(to: View.self) { songs in
+                let context = UserContext(title: user.name, user: user, songs: songs.isEmpty ? nil: songs)
+                return try req.leaf().render("user",context)
             }
         }
     }
@@ -42,15 +54,21 @@ extension Request {
 }
 
 // Leaf dosyasının kullanacağı bir yapı oluşturduk.
-struct IndexContent: Encodable {
+struct IndexContext: Encodable {
     let title: String
     // songs dizisi oluşturduk
     let songs: [Song]?
 }
 
 // SongHandler için hazırladığımız content yapısı
-struct SongContent: Encodable {
+struct SongContext: Encodable {
     let title: String
     let song: Song
     let creator: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let songs: [Song]?
 }
